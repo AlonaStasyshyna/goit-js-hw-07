4f3c2e3d55a1254829b9607695f6e92148846f88
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-08-31 19:07:43 +0300

    Add HW 7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
 const gallery = document.querySelector(".gallery");
 const itemsOfGallary = createGalleryItems(galleryItems);
+let instance;
 
-gallery.insertAdjacentHTML("beforeend", itemsOfGallary);
+gallery.innerHTML = itemsOfGallary;
 
 function createGalleryItems(items) {
   return items
@@ -35,25 +29,32 @@ function createGalleryItems(items) {
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
 
-  if (event.target.tagName !== "IMG") {
+  if (!event.target.classList.contains("gallery__image")) {
     return;
   }
 
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(
+    `
         <img
-        class="gallery__image"
-        src="${event.target.dataset.source}"/>
-    `);
+        src="${event.target.dataset.source}" 
+        alt="${event.target.alt}"/>
+        
+    `,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onEsc);
+      },
+    }
+  );
   instance.show();
 });
 
-//   document.querySelectorAll(".gallery__image").forEach(function (img) {
-//     if (event.target.dataset.source === img.dataset.source) {
-//       const instance = basicLightbox.create(`
-//       <img
-//       class="gallery__image"
-//       src="${event.target.dataset.source}"/>
-//   `);
-//       instance.show();
-//     }
-//   });
+function onEsc(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  instance.close();
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const itemsOfGallary = createGalleryItems(galleryItems);
+let instance;
+
+gallery.innerHTML = itemsOfGallary;
+
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+            </a>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
